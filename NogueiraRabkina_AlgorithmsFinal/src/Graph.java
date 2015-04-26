00f7777612fb3d7efe8ed547706190f4af0c330a
Irina Rabkina <irabkina@gmail.com>
2015-04-26 15:28:18 -0500

    test

--- NogueiraRabkina_AlgorithmsFinal/src/Graph.java
+++ NogueiraRabkina_AlgorithmsFinal/src/Graph.java
@@ -31,7 +31,7 @@ public class Graph {
 		Graph toReturn = new Graph();
 		HashMap<Integer, ArrayList<Integer>> vals = new HashMap<Integer, ArrayList<Integer>>();
 		for(Entry<Integer, ArrayList<Integer>> entry : this.getEdges().entrySet()){
-			vals.put(entry.getKey(), entry.getValue()); 
+			vals.put(entry.getKey(), entry.getValue()); //kljlkj
 		}
 		
 		toReturn.setEdges(vals);
